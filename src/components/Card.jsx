d45a34f99ca81783cfbe73f68dea49e8da5b8695
erik.minasian <erik.minasian@coliquio.de>
2024-03-13 17:24:37 +0400

    feat: information display for cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,15 @@
+import { FIRST_STAGE, LAST_STAGE } from "../utils/constants"
+
 export default function Card({ task }) {
+    const { name, description, stage } = task
     return (
         <div>
-
+            <h3>{name}</h3>
+            <span>{description}</span>
+            <div>
+                {stage !== LAST_STAGE && <button>Upgrade</button>}
+                {stage !== FIRST_STAGE && <button>Downgrade</button>}
+            </div>
         </div>
     )
 }
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const FIRST_STAGE = 1
+export const LAST_STAGE = 5
